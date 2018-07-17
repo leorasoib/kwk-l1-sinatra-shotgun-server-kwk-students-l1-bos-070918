9a5316cb110b0d92e0bf3ae15c21e292ba9d9e57
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-07-17 13:48:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "We met Karlie! She posted our pictures online!YeeeAHHHHHHH"
   end
-
+get '/kitty' do
+  "Hello Kitty!"
+end 
+get '/lollipop' do
+  "I like Ice Cream!"
+end 
 end
